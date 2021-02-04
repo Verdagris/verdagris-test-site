042c2c1f099f78468d2d8e07eb69d2ff959c8270
Verdagris <eglint@gmail.com>
2021-02-04 23:09:54 +0200

    update clouds

--- src/pages/art.js
+++ src/pages/art.js
@@ -1,9 +1,21 @@
 import React from "react"
 import Layout from "../Components/layout"
+import art from "../Styles/art.scss"
 
 const ArtPage = () => {
     return(
+
+
     <Layout>
+        <div className="cloud-circle"></div>
+
+        <svg width = "0" height = "0">
+            <filter id="filter">
+                <feTurbulence type="fractalNoise" baseFrequency=".01" numOctaves = "10"/>
+                <feDisplacementMap in="SourceGraphic" scale = "180" />
+            </filter>
+        </svg>
+
         <h2>Pics and Paintings</h2>
         <p>See the world through my lens</p>
     </Layout>
